3956a0789f952d9a8b403fba90c5d04c0ded14e6
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-14 20:41:59 +0400

    fixed space in mentor entry

--- src/components/entries/MentorEntry.vue
+++ src/components/entries/MentorEntry.vue
@@ -15,23 +15,25 @@ let user = users.find(user => user.id === props.entry.author_id)
 </script>
 
 <template>
-  <EntryContainer>
-    <v-row 
-      @click="router.push('account/?_id=${user.id}')" 
-      class="flex-row ma-0 pa-0 flex-nowrap align-center justify-start cursor-pointer"
-    >
-      <v-avatar 
-        :image="user?.avatar_url"
-        color="blue"
-      />
-      <div class="d-flex ml-4 flex-column justify-start">
-        <div class="font-author font-weight-semibold">{{ user?.name }}</div>
-        <div class="text-gray text-body-2">{{ user?.achievements.join(', ') }}</div>
-      </div>
-    </v-row>
-
-    <div class="text-h6 mt-2 font-weight-black">{{ entry.subject }}</div>
-    <div class="mt-1">{{ entry.description }}</div>
+  <EntryContainer class="justify-space-between">
+    <div class="d-flex w-100 justify-start flex-column">
+      <v-row 
+        @click="router.push('account/?_id=${user.id}')" 
+        class="flex-row ma-0 pa-0 flex-nowrap align-center justify-start cursor-pointer"
+      >
+        <v-avatar 
+          :image="user?.avatar_url"
+          color="blue"
+        />
+        <div class="d-flex ml-4 flex-column justify-start">
+          <div class="font-author font-weight-semibold">{{ user?.name }}</div>
+          <div class="text-gray text-body-2">{{ user?.achievements.join(', ') }}</div>
+        </div>
+      </v-row>
+  
+      <div class="text-h6 mt-2 font-weight-black">{{ entry.subject }}</div>
+      <div class="mt-1">{{ entry.description }}</div>
+    </div>
 
     <div style="gap: 4px;" class="d-flex mt-4 flex-row flex-wrap justify-start align-center">
       <v-btn 
