b2c96655154b6a9f6663599d6622a706f7ba4750
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-14 20:46:34 +0400

    added random in mentor search

--- src/pages/searching/MentorsSearchPage.vue
+++ src/pages/searching/MentorsSearchPage.vue
@@ -2,12 +2,25 @@
 import BackButton from '@/components/BackButton.vue'
 import MentorEntry from '../../components/entries/MentorEntry.vue'
 import { mentorship_entries } from '../../fakeDB/mentorship-entries';
+
+function shuffle(arr: any[]){
+	var j, temp;
+	for(var i = arr.length - 1; i > 0; i--){
+		j = Math.floor(Math.random()*(i + 1));
+		temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+	}
+	return arr;
+}
+
+shuffle(mentorship_entries)
 </script>
 
 <template>
   <v-container class="mt-1">
     <BackButton />
-    <div class="text-h4 mt-1 font-weight-bold">Поиск наставнков</div>
+    <div class="text-md-h4 text-sm-h5 mt-1 font-weight-bold">Поиск наставнков</div>
     <v-btn class="mt-4 text-capitalize text-body-1" variant="tonal" prepend-icon="mdi-filter">Фильтр</v-btn>
 
     <!-- Не показывать если забанили
