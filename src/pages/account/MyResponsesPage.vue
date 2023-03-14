2b26defbc0c9b60c8aee0b73c74e29cafa777308
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-14 21:31:59 +0400

    added my responses page

--- src/pages/account/MyResponsesPage.vue
+++ src/pages/account/MyResponsesPage.vue
@@ -1,7 +1,40 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+import BackButton from '../../components/BackButton.vue';
+import MentorEntry from '../../components/entries/MentorEntry.vue';
+import { mentorship_entries } from '../../fakeDB/mentorship-entries';
 
+let router = useRouter()
+
+let mentorship = mentorship_entries.filter(entry => entry.responses.includes('640f4b30cde42820d332b0e9'))
+// С уроками и кружками так же
 </script>
 
 <template>
-  my responces
+  <v-container class="mt-1">
+    <BackButton />
+    <div class="text-h4 mt-1 font-weight-bold">Мои отклики</div>
+
+    <v-row class="mt-5">
+      <v-col 
+        v-for="entry in mentorship" 
+        :key="entry?.id"
+        cols="12" sm="6" xs="12"
+      >
+        <MentorEntry :entry="entry" />
+      </v-col>
+    </v-row>
+
+    <div class="d-flex mt-3 align-center ma-auto flex-column justify-center" v-if="mentorship.length==0">
+      <div class="text-h4 text-center font-weight-semibold">У вас нет наставников 😱</div>
+      <v-btn 
+        size="small"
+        variant="tonal" 
+        class="text-body-2 pl-5 pr-5 mt-4 font-weight-semibold bg-button"
+        @click="router.push('/searchMentors')"
+      >
+        Найти наставника
+      </v-btn>
+    </div>
+  </v-container>
 </template>
