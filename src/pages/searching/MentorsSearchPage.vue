0629024557802716a38cabaf6e03e488a9457021
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-17 23:10:37 +0400

    added filter in mentors search page

--- src/pages/searching/MentorsSearchPage.vue
+++ src/pages/searching/MentorsSearchPage.vue
@@ -3,6 +3,9 @@ import BackButton from '@/components/BackButton.vue'
 import MainTitle from '../../components/MainTitle.vue';
 import MentorEntry from '../../components/entries/MentorEntry.vue'
 import { mentorship_entries } from '../../fakeDB/mentorship-entries';
+import { ref } from 'vue';
+import { towns } from '../../fakeDB/towns';
+import { schools } from '../../fakeDB/schools';
 
 function shuffle(arr: any[]){
 	var j, temp;
@@ -16,29 +19,66 @@ function shuffle(arr: any[]){
 }
 
 shuffle(mentorship_entries)
+
+// КОСТЫЛЬ
+// Надо по умолчанию город и школу пользователя
+let town_id = ref('640f4ac9145a0da782eb1a95')
+let school_id = ref('640f4af989029a9d95db4b19')
+
+let school = (): any => { return schools.find(sch => sch.id === school_id.value) }
+let schools_in_town = (): any => { return towns.find(town => town.id === town_id.value)?.schools }
 </script>
 
 <template>
   <v-container>
     <MainTitle>Поиск наставников</MainTitle>
-    <v-btn class="mt-4 text-capitalize text-body-1" variant="tonal" prepend-icon="mdi-filter">Фильтр</v-btn>
 
-    <!-- Не показывать если забанили
+    <!-- Filter -->
+    <v-row class="ma-0 pa-0 mt-4" style="gap: 12px;">
+      <v-col cols="12" md="4" sm="8" xs="12" class="ml-0 pa-0">
+        <v-select
+          label="Город"
+          v-model="town_id"
+          :items="towns"
+          item-title="name"
+          item-value="id"
+          variant="solo"
+          hide-details
+        ></v-select>
+      </v-col>
+      <v-col cols="12" md="4" sm="8" xs="12" class="ma-0 pa-0">
+        <v-select
+          label="Школа"
+          v-model="school_id"
+          :items="[{name: 'Все', id: 'all'}, ...schools_in_town().map(el => el = schools.find(i => i.id === el))]"
+          item-title="name"
+          item-value="id"
+          variant="solo"
+          hide-details
+        ></v-select>
+      </v-col>
+    </v-row>
+    
+    <!-- Собирать с town_id т е забирать не всё
+         Не показывать если забанили
          Пересылать на авторизацию если не вошли
          Всегда применять фильтр по городу и школе челика
          Не показывать у самого челика
-         Добавить лайки
          Не показывать если чел стал наставляемым
          Расставляем в рандомном порядке - имитируем индивидуальный подбор))))
     -->
     <v-row class="mt-5">
-      <v-col 
+      <template
         v-for="entry in mentorship_entries" 
         :key="entry.id"
-        cols="12" sm="6" xs="12"
       >
-        <MentorEntry :entry="entry" />
-      </v-col>
+        <v-col 
+          v-if="entry.school_id == school_id || (entry.town_id === town_id && school_id === 'all')"
+          cols="12" sm="6" xs="12"
+        >
+          <MentorEntry :entry="entry" />
+        </v-col>
+      </template>
     </v-row>
   </v-container>
 </template>
