36370ced235d71aa248d0c9e71a02a24004be5cc
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-19 17:30:16 +0400

    added adaptive padding in enry container

--- src/components/entries/EntryContainer.vue
+++ src/components/entries/EntryContainer.vue
@@ -3,14 +3,13 @@
 </script>
 
 <template>
-  <div class="box w-100 h-100 d-flex flex-column justify-start">
+  <div class="box w-100 h-100 pl-6 pr-6 pt-4 pb-4 pl-md-7 pr-md-7 pt-md-5 pb-md-5 d-flex flex-column justify-start">
     <slot />
   </div>
 </template>
 
 <style lang="scss" scoped>
 .box {
-  padding: 17px 28px;
   background: #FFFFFF;
   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
   border-radius: 12px;
