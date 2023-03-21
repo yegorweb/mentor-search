11879fdbb4696b9078307b7ef205480396db32e5
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-21 21:49:44 +0400

    added vue-toastification

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import { registerPlugins } from './plugins/index'
+import Toast from "vue-toastification"
+import "vue-toastification/dist/index.css"
 
 const app = createApp(App)
 
 registerPlugins(app)
 
 app.mount("#app")
+app.use(Toast)
