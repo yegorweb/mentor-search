b6599f36f4eddaa52d7d2dbd57e7c54e97222f5c
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-22 17:06:02 +0400

    updated school service

--- src/services/SchoolService.js
+++ src/services/SchoolService.js
@@ -3,5 +3,8 @@ import $api from "../plugins/axios"
 export default {
   async get_by_id(_id) {
     return $api.get('/school/get-by-id', _id)
+  },
+  async get_all_in_town(_id) {
+    return $api.get('/school/get-all-in-town', _id)
   }
 }
