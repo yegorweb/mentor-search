bae7f06c4e8b21631a2699a1e61c3428ccda90c4
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-22 13:58:14 +0400

    added api plugin

---
+++ src/plugins/axios.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { useToast } from 'vue-toastification'
+const toast = useToast()
+
+export const API_URL = import.meta.env.VITE_API_URL
+
+const $api = axios.create({
+	withCredentials: true,
+	baseURL: API_URL
+})
+
+// for jwt auth
+$api.interceptors.request.use((config) => {
+	config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+	return config
+})
+
+$api.interceptors.response.use(
+	function (response) {
+		toast.success(response.data?.message)
+
+		return response
+	}, 
+	function (error) {
+		toast.error(error.response?.data?.message)
+		console.log("ERROR: ", error)
+
+		return error
+	}
+)
+
+export default $api
