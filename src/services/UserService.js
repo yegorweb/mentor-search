b0ee36a46f06c3ac3df4df582571301f62936b00
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-23 14:06:42 +0400

    updated user service

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,5 +3,8 @@ import $api from '../plugins/axios'
 export default {
   async get_by_id(_id) {
     return $api.get('/user/get-by-id', _id)
+  },
+  async get_my_responses() {
+    return $api.get('/user/get-my-responses', localStorage.getItem('token'))
   }
 }
