b29c777e497b807074e6eaed78742f4e7b1fe0a9
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-23 15:12:26 +0400

    updated school service

--- src/services/SchoolService.js
+++ src/services/SchoolService.js
@@ -6,5 +6,11 @@ export default {
   },
   async get_all_in_town(_id) {
     return $api.get('/school/get-all-in-town', _id)
-  }
+  },
+  async create(name) {
+    return $api.post('school/create', { name, token: localStorage.getItem('token') })
+  },
+  async switch_to_active(_id) {
+    return $api.post('school/switch-to-active', { _id, token: localStorage.getItem('token') })
+  }  
 }
