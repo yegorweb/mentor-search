0c8db3fc0be9eb62c9e8f778abbeea0986efecf6
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-23 15:12:41 +0400

    updated town service

--- src/services/TownService.js
+++ src/services/TownService.js
@@ -6,5 +6,8 @@ export default {
   },
   async getTownById(_id) {
     return $api.get('/town/get-by-id', _id)
+  },
+  async create(name) {
+    return $api.post('/town/create', { name, token: localStorage.getItem('token') })
   }
 }
