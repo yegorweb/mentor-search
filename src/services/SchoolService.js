c1dfbecddaa1577274309b79e93835ab601fdfa1
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-24 17:25:21 +0400

    updated school service (added get all)

--- src/services/SchoolService.js
+++ src/services/SchoolService.js
@@ -4,6 +4,9 @@ export default {
   async get_by_id(_id) {
     return $api.get('/school/get-by-id', _id)
   },
+  async get_all() {
+    return $api.get('/school/get-all')
+  },
   async get_all_in_town(_id) {
     return $api.get('/school/get-all-in-town', _id)
   },
