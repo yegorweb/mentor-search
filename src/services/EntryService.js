f37a3f196526bd3e64b65ac37d920b61d0029432
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-24 17:25:35 +0400

    fixed entry service

--- src/services/EntryService.js
+++ src/services/EntryService.js
@@ -2,18 +2,18 @@ import $api from "../plugins/axios"
 
 export default {
   async get(type, town_id, school_id) {
-    $api.get('/entry/get', { type, town_id, school_id })
+    return $api.get('/entry/get', { type, town_id, school_id })
   },  
   async get_by_id(_id) {
-    $api.get('/entry/get-by-id', { _id })
+    return $api.get('/entry/get-by-id', { _id })
   },
   async create(entry) {
-    $api.post('/entry/create', { entry, token: localStorage.getItem('token') })
+    return $api.post('/entry/create', { entry, token: localStorage.getItem('token') })
   },
   async update(entry) {
-    $api.post('/entry/update', { entry, token: localStorage.getItem('token') })
+    return $api.post('/entry/update', { entry, token: localStorage.getItem('token') })
   },
   async delete(entry) {
-    $api.delete('/entry/delete', { entry, token: localStorage.getItem('token') })
+    return $api.delete('/entry/delete', { entry, token: localStorage.getItem('token') })
   },
 }
