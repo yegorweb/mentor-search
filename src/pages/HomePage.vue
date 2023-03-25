2eb0f42b18603cfd8589046a6cae2724c5505b32
Yegor Knyazev <egorknyazev10@gmail.com>
2023-03-25 09:53:08 +0400

    added checking auth in home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,32 +1,62 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router"
-
+import { useAuth } from '../stores/auth'
+ 
 document.title = 'Ищу наставника'
 
-let buttons = [
+let auth = useAuth()
+let user = auth.getUser()
+
+let isAuth = auth.getAuthStatus()
+
+let buttons = isAuth ? [
   {
     route: '/searchMentors',
     text: 'Найти наставника',
-  },
+  },  
   {
     route: '/settings',
     text: 'Стать наставником',
-  },
+  },  
   {
     route: '/myResponses',
     text: 'Мои отклики',
   },
+] : [
+  {
+    route: '/login',
+    text: 'Авторизоваться',
+  },
+  {
+    route: '/searchMentors',
+    text: 'Посмотреть наставников',
+  },  
 ]
+
 let router = useRouter()
+
+function getType(): string {
+  if (user?.roles.includes('school-admin') || user?.roles.includes('global-admin')) {
+    return 'админ'
+  }
+  if (user?.roles.includes('mentor')) {
+    return 'наставник'
+  }
+  if (!isAuth) {
+    return 'войдите или зарегистрируйтесь'
+  }
+
+  return 'наставляемый'
+}
 </script>
 
 <template>
   <v-container>
     <v-row class="align-center ma-0 pa-0 mt-5 flex-nowrap">
-      <v-avatar size="60" color="blue" />
+      <v-avatar :url="user?.avatar_url" size="60" color="blue" />
       <div class="d-flex ml-4 flex-column justify-start">
-        <div class="font-weight-bold text-h5">Привет, Василий Пупкин</div>
-        <div class="text-body-3 text-text_gray">наставляемый</div>
+        <div class="font-weight-bold text-h5">{{ auth.getAuthStatus() ? `Привет, ${user?.name}` : 'Вы не представились'}}</div>
+        <div class="text-body-3 text-text_gray">{{ getType() }}</div>
       </div>
     </v-row>
 
