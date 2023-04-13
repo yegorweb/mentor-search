2094ff9f70b64b82366d5750b7e82b8b28f74bea
Yegor Knyazev <egorknyazev10@gmail.com>
2023-04-13 11:51:10 +0400

    fixed checking auth

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { registerPlugins } from './plugins/index'
 import Toast from "vue-toastification"
 import "vue-toastification/dist/index.css"
 import { createPinia } from "pinia"
+import router from './router'
+import { useAuth } from "./stores/auth"
 
 const pinia = createPinia()
 
@@ -11,5 +13,9 @@ const app = createApp(App)
 
 registerPlugins(app)
 
-app.mount("#app")
 app.use(Toast).use(pinia)
+
+await useAuth().checkAuth()
+
+app.use(router)
+app.mount("#app")
