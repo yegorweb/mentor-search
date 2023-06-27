913eb1dd248b2f2fe67ad4cc5d6cbc72730c5751
Yegor Knyazev <egorknyazev10@gmail.com>
2023-06-27 17:17:45 +0400

    added close button component

---
+++ src/components/CloseButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps({
+  modelValue: {
+    type: Boolean,
+    required: true,
+    default: null
+  }
+})
+
+let emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <v-icon 
+    @click="emit('update:modelValue', false)"
+    class="pa-4"
+    style="position: absolute; top: 4px; right: 4px;"
+  >
+    mdi-close
+  </v-icon>
+</template>
