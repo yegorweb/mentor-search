7a20ea524156b10ab7efd60e0f4925278f3d63f5
Yegor Knyazev <egorknyazev10@gmail.com>
2023-08-09 15:05:49 +0400

    updated user interface

--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -1,7 +1,5 @@
 import { Achievement } from "./achievement.type"
-import { Answer } from "./answer.type"
 import { Contact } from "./contact.type"
-import { Roles } from "./role.type"
 import School from "./school.interface"
 import Town from "./town.interface"
 
@@ -14,11 +12,10 @@ export interface User {
   avatar_url?: string
   ranks: string[]
   achievements: Achievement[]
-  answers: Answer[]
   contacts: Contact[]
   grade: number
   town: Town
   school: School
   date: number
-  roles: Roles
+  roles: string[]
 }
