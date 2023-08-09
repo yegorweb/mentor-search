805c6fb57ab4434fc9b6cfea7edc0e25ef457424
Yegor Knyazev <egorknyazev10@gmail.com>
2023-08-09 14:43:00 +0400

    reviewed BackButton

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -5,8 +5,14 @@ let router = useRouter()
 </script>
 
 <template>
-  <div @click="router.back()" class="d-flex pt-1 pr-1 pb-1 cursor-pointer flex-row flex-nowrap align-center justify-start">
-    <v-icon icon="mdi-arrow-left"></v-icon>
-    <div class="text-body-4 ml-1 font-weight-semibold">назад</div>
+  <div 
+    @click="router.back()" 
+    class="d-flex pt-1 pr-1 pb-1 cursor-pointer flex-row flex-nowrap align-center justify-start"
+  >
+    <v-icon>mdi-arrow-left</v-icon>
+    
+    <div 
+      class="text-body-4 ml-1 font-weight-semibold"
+    >назад</div>
   </div>
 </template>
