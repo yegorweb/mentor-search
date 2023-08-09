17e50515de93f3913ebe2e6c5cb1db7628ffcffe
Yegor Knyazev <egorknyazev10@gmail.com>
2023-08-09 14:51:56 +0400

    reviewed school page

--- src/pages/admin/SchoolPage.vue
+++ src/pages/admin/SchoolPage.vue
@@ -1,27 +1,31 @@
-<script setup>
+<script setup lang="ts">
 import MainTitle from '@/components/MainTitle.vue'
 import Slave from '@/components/Slave.vue'
 import { useSchool } from '../../stores/school'
 import { useUser } from '../../stores/user'
 import { useAuth } from '../../stores/auth'
 import { onBeforeRouteUpdate } from 'vue-router'
+import { User } from '../../types/user.interface'
+import School from '../../types/school.interface'
 
 let props = defineProps(['id'])
 let id = props.id
 
-let school = await useSchool().get_by_id(id)
-let users = (await useUser().get_all_by_school(id)).filter(item => item._id !== useAuth().getUser()._id)
+let user = useAuth().user as User
 
-onBeforeRouteUpdate(() => {
+let school = await useSchool().get_by_id(id) as School
+let users = (await useUser().get_all_by_school(id)).filter(item => item._id !== user._id)
+
+onBeforeRouteUpdate((to) => {
   window.location.href = to.path
 })
 </script>
 
 <template>
   <v-container>
-    <MainTitle>{{ school.town.name +', '+ school.name }}</MainTitle>
+    <MainTitle>{{ school.town.name + ', ' + school.name }}</MainTitle>
 
-    <v-row class="mt-8">
+    <v-row class="mt-4">
       <v-col
         v-for="user in users"
         :key="user._id"
