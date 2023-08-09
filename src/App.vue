09375ca099b058f463170b850c766ae26c18083a
Yegor Knyazev <egorknyazev10@gmail.com>
2023-08-09 14:39:49 +0400

    reviewed App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <script setup lang="ts">
-import Responses from '@/components/Responses.vue'
 </script>
 
 <template>
   <Suspense>
     <router-view></router-view>
   </Suspense>
-  <Suspense>
-    <Responses></Responses>
-  </Suspense>
 </template>
-
-<style lang="scss">
-
-</style>
