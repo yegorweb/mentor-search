ef49ee9eb2b3c72ea80e455099b321bfa0c1d1b0
Yegor Knyazev <egorknyazev10@gmail.com>
2023-08-09 15:05:00 +0400

    removed responses showing store

--- src/stores/responsesShowing.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from "pinia";
-import { Ref, ref } from "vue";
-
-export const useResponsesShowing = defineStore('responsesShowing', () => {
-  let showing = ref(false)
-  let responses = ref<string[]>([])
-
-  return { showing, responses }
-})
