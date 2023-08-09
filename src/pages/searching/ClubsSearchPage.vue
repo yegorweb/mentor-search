d2d5a368cca4eea0a4ea6f3883da963e8b1a895b
Yegor Knyazev <egorknyazev10@gmail.com>
2023-08-09 15:02:05 +0400

    reviewed searching pages

--- src/pages/searching/ClubsSearchPage.vue
+++ src/pages/searching/ClubsSearchPage.vue
@@ -2,7 +2,7 @@
 import BackButton from '@/components/BackButton.vue'
 import MainTitle from '../../components/MainTitle.vue';
 import MentorEntry from '../../components/entries/MentorEntry.vue'
-import { ref, Ref } from 'vue';
+import { ref, watch } from 'vue';
 import { useAuth } from '../../stores/auth';
 import { useSchool } from '../../stores/school';
 import { useTown } from '../../stores/town';
@@ -10,51 +10,49 @@ import { useEntry } from '../../stores/entry';
 import Town from '../../types/town.interface';
 import School from '../../types/school.interface';
 import Entry from '../../types/entry.interface';
+import _ from 'lodash'
 
 document.title = 'Поиск клубов — Ищу наставника'
 
 let auth = useAuth()
 let user = auth.getUser()
-
-function shuffle(arr: any[]){
-	var j, temp;
-	for(var i = arr.length - 1; i > 0; i--){
-		j = Math.floor(Math.random()*(i + 1));
-		temp = arr[j];
-		arr[j] = arr[i];
-		arr[i] = temp;
-	}
-	return arr;
-}
-
 let townStore = useTown()
 let schoolStore = useSchool()
 let entryStore = useEntry()
 
 let towns: Town[] = await townStore.get_all() as any
-let schools: School[] = await schoolStore.get_all() as any
 
-let town: Ref<Town> = ref(user ? 
+
+let town = ref<Town>(user ? 
   user.town : 
   towns.find(town => town.name === 'Глазов') as any
 )
 
-let school = ref(user ? 
+let school = ref<School | null>(user ? 
   user.school : 
-  { name: 'Все', _id: 'all' }
+  { name: 'Все', _id: 'all' } as any
 )
 
-let mentorship_entries: Ref<Entry[]> = ref(await entryStore.get('club', town.value._id, school.value._id) as any)
-shuffle(mentorship_entries.value)
+let entries = ref<Entry[]>([])
 
 async function updateEntries() {
-  mentorship_entries.value = await entryStore.get('club', town.value._id, school.value._id) as any
-  shuffle(mentorship_entries.value)
+  entries.value = school.value ? await entryStore.get('club', town.value._id, school.value._id) : []
 }
+updateEntries()
 
-function schools_in_town() {
-  return schools.filter(sch => sch.town._id === town.value._id)
-}
+let schools_in_town = ref(await schoolStore.get_all_in_town(town.value._id))
+
+watch(town, async (new_value, old_value) => {
+  if (_.isEqual(new_value, old_value)) return
+
+  school.value = null
+  schools_in_town.value = await schoolStore.get_all_in_town(town.value._id)
+})
+watch(school, (new_value, old_value) => {
+  if (_.isEqual(new_value, old_value)) return
+
+  updateEntries()
+})
 </script>
 
 <template>
@@ -62,50 +60,64 @@ function schools_in_town() {
     <MainTitle>Поиск клубов</MainTitle>
 
     <!-- Filter -->
-    <v-row class="ma-0 pa-0 mt-4" style="gap: 12px;">
-      <v-col cols="12" md="4" sm="8" xs="12" class="ml-0 pa-0">
-        <v-select
+    <v-row class="mt-4" style="margin: -6px;">
+      <v-col 
+        cols="12" md="4" sm="8" xs="12" 
+        style="padding: 6px;"
+      >
+        <v-autocomplete
           label="Город"
           v-model="town"
           :items="towns"
           item-title="name"
+          auto-select-first
           return-object
           variant="solo"
           hide-details
-        ></v-select>
+        />
       </v-col>
 
-      <v-col cols="12" md="4" sm="8" xs="12" class="ma-0 pa-0">
-        <v-select
+      <v-col 
+        cols="12" md="4" sm="8" xs="12" 
+        style="padding: 6px;"
+      >
+        <v-autocomplete
           label="Школа"
           v-model="school"
-          :items="[{ name: 'Все', _id: 'all' }, ...schools_in_town()]"
+          :items="[{ name: 'Все', _id: 'all' }, ...schools_in_town]"
           item-title="name"
+          auto-select-first
           return-object
-          @update:model-value="updateEntries"
           variant="solo"
           hide-details
-        ></v-select>
+        />
       </v-col>
     </v-row>
     
     <div class="mt-8 entries-container">
       <Suspense>
         <MentorEntry 
-          v-for="entry in mentorship_entries"
+          v-for="entry in entries"
           :key="entry._id"
           :entry="entry" 
-          :show_location="school._id === 'all'" 
+          :show_location="school?._id === 'all'" 
         />
       </Suspense>
     </div>
 
-    <v-col 
-      cols="12" sm="6" xs="12" 
-      class="text-h6 ma-auto font-weight-medium mt-4 text-center"
-      v-if="mentorship_entries.length === 0"
+    <div
+      v-if="school && entries.length === 0"
+      class="w-100 text-center text-h6 font-weight-medium"
     >
-      Клубов нет, или вы на всё откликнулись
-    </v-col>
+      Пусто
+    </div>
+    
+    <div 
+      v-if="!school"
+      class="w-100 text-center text-h6 font-weight-medium"
+    >
+      Выберите школу
+    </div>
   </v-container>
 </template>
+
