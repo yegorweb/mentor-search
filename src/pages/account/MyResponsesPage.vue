111c645a30f571ac57aa34fcb337e8d9e822738a
Yegor Knyazev <egorknyazev10@gmail.com>
2023-12-23 17:32:48 +0400

    removed splitting in my repsonses

--- src/pages/account/MyResponsesPage.vue
+++ src/pages/account/MyResponsesPage.vue
@@ -10,28 +10,17 @@ document.title = 'Мои отклики — Ищу наставника'
 let userStore = useUser()
 
 let entries = ref<Entry[]>([])
-let mentorship_entries = ref<Entry[]>([])
-let lesson_entries = ref<Entry[]>([])
-let club_entries = ref<Entry[]>([])
 let loading = ref(false)
-
-function updateEntries() {
-  mentorship_entries.value = entries.value.filter(entry => entry.type === 'mentor')
-  lesson_entries.value = entries.value.filter(entry => entry.type === 'lesson')
-  club_entries.value = entries.value.filter(entry => entry.type === 'club')
-}
 
 async function fetchEntries() {
   loading.value = true
   entries.value = await userStore.get_my_responses()
-  updateEntries()
   loading.value = false 
 }
 fetchEntries()
 
 function onDeleteEntry(_id: string) {
   entries.value = entries.value.filter(entry => entry._id !== _id)
-  updateEntries()
 }
 </script>
 
@@ -48,80 +37,34 @@ function onDeleteEntry(_id: string) {
       />
     </div>
 
-    <template v-if="!loading">
-      <div
-        v-if="mentorship_entries.length > 0" 
-        class="entries-container pt-4 pb-4"
-      >
-        <MentorEntry 
-          v-for="entry in mentorship_entries"
-          @delete="onDeleteEntry"
-          :key="entry._id"
-          :entry="entry" 
-        />
+    <div 
+      v-else-if="!loading && entries && entries.length"
+      class="entries-container pt-4 pb-4"
+    >
+      <MentorEntry 
+        v-for="entry in entries"
+        @delete="onDeleteEntry"
+        :key="entry._id"
+        :entry="entry" 
+      />
+    </div>
+
+    <div 
+      v-else
+      class="d-flex mt-3 flex-column align-center align-sm-start" 
+    >
+      <div class="text-h5 w-100 text-center font-weight-semibold">
+        У вас нет откликов 😱
       </div>
-
-      <!-- Lessons -->
-      <div
-        v-if="lesson_entries.length > 0"
-        class="d-flex flex-column pt-4 pb-4"
+      
+      <v-btn 
+        size="small"
+        variant="tonal" 
+        class="text-body-2 pl-5 pr-5 ma-auto mt-4 font-weight-semibold bg-button"
+        to="/search-mentors"
       >
-        <div 
-          v-if="mentorship_entries.length > 0"
-          class="text-h5 mb-4 font-weight-bold" 
-        >
-          Уроки
-        </div>
-
-        <div class="entries-container">
-          <MentorEntry 
-            v-for="entry in lesson_entries"
-            @delete="onDeleteEntry"
-            :key="entry._id"
-            :entry="entry" 
-          />
-        </div>
-      </div>
-
-      <!-- Clubs -->
-      <div
-        v-if="club_entries.length > 0"
-        class="d-flex flex-column pt-4"
-      >
-        <div 
-          v-if="mentorship_entries.length > 0 || lesson_entries.length > 0"
-          class="text-h5 mb-4 font-weight-bold" 
-        >
-          Клубы
-        </div>
-
-        <div class="entries-container">
-          <MentorEntry 
-            v-for="entry in club_entries"
-            @delete="onDeleteEntry"
-            :key="entry._id"
-            :entry="entry" 
-          />
-        </div>
-      </div>
-
-      <div 
-        v-if="entries.length === 0"
-        class="d-flex mt-3 flex-column align-center align-sm-start" 
-      >
-        <div class="text-h5 w-100 text-center font-weight-semibold">
-          У вас нет откликов 😱
-        </div>
-        
-        <v-btn 
-          size="small"
-          variant="tonal" 
-          class="text-body-2 pl-5 pr-5 ma-auto mt-4 font-weight-semibold bg-button"
-          to="/search-mentors"
-        >
-          Найти наставника
-        </v-btn>
-      </div>
-    </template>
+        Найти наставника
+      </v-btn>
+    </div>
   </v-container>
 </template>
